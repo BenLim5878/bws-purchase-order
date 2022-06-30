dfd99f628c8f6e047461ea123f4d7c285462c4f8
Ben Lim <limzhengwei1002@gmail.com>
2022-06-30 23:42:25 +0800

    first commit

---
+++ Helper.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+void hidecursor() {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(consoleHandle, &info);
+}
